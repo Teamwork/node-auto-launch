c25a9d6b185c292b2aa1866d4889534932c24387
Alexandre Demers <alexandre.f.demers@gmail.com>
2023-12-28 23:02:57 -0500

    Remove decaffeinate comments
    Signed-off-by: Alexandre Demers <alexandre.f.demers@gmail.com>

--- tests/helper.js
+++ tests/helper.js
@@ -1,8 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 let AutoLaunchHelper;
 module.exports = (AutoLaunchHelper = class AutoLaunchHelper {
     constructor(autoLaunch) {
--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
 let executablePath, isLinux;
 const chai = require('chai');
 const path = require('path');
